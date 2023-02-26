12a0d80484952acbf9447da976d3acb5b17ed738
lukivan8 <lukivan888@gmail.com>
2023-02-26 15:25:37 +0600

    Added main == name checker

--- script.py
+++ script.py
@@ -4,21 +4,21 @@ import keyboard
 import pyautogui
 
 
-def find_click():
-    try:
-        points = pyautogui.locateOnScreen("icon.png",
-                                          confidence=0.8,
-                                          region=(197, 125, 825, 864),
-                                          grayscale=True)
-        win32api.SetCursorPos((points.left + points.width // 2 - 10,
-                               points.top + points.height // 2))
-        time.sleep(0.1)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-        time.sleep(0.5)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-    except Exception as error:
-        print(error)
+if __name__ == "__main__":
+    while not (keyboard.is_pressed("q")):
+        try:
+            points = pyautogui.locateOnScreen("icon.png",
+                                            confidence=0.8,
+                                            region=(197, 125, 825, 864),
+                                            grayscale=True)
+            win32api.SetCursorPos((points.left + points.width // 2 - 10,
+                                points.top + points.height // 2))
+            time.sleep(0.1)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+            time.sleep(0.5)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+        except Exception as error:
+            print(error)
 
 
-while not (keyboard.is_pressed("q")):
-    find_click()
+
